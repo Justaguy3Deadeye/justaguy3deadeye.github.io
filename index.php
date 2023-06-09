e969769399ac0c22e7e29c5cff9a07c0faed11b9
Justaguy3Deadeye <a214207849@unison.mx>
2023-06-08 19:10:37 -0700

    stinky

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Historias de dnd</title>
+    <link rel="preload" href="css/normalize.css" as="style">
+    <link rel="stylesheet" href="css/normalize.css">
+        <link rel="preload" href="css/styles.css" as="style">
+        <link href="css/styles.css" rel="stylesheet">
+    </head>
+<body>
+    <main>
+    <header>
+        <h1>Adventureros y sus historias</h1>
+    </header>
+    
+  
+    <section class="intro">
+        <p><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-help" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <circle cx="12" cy="12" r="9" />
+        <line x1="12" y1="17" x2="12" y2="17.01" />
+        <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
+      </svg></p>
+        <h2>What is this page for</h2>
+       
+        <p class="content">This page is a glosary of all the characters i have palyed or created so far.</p>
+        <p class="content">Each one comes with their own storys and what happend in their point of view on their adventures</p>   
+       </section>
+<section>
+</main>
+    <section>
+        <p>
+            
+        </p>
+    </section>
+    <nav class="content">
+        <a href="#">Marcus the ranger</a><br>
+        <br><a href="#">Greerer the bard</a><br>
+        <br><a href="#">tsukioshi the werewolf</a><br>
+        <br><a href="#">Astyr the paladin</a><br>
+        <br><a href="#">TaK the gentelman</a><br>
+        <br> <a href="#">Theodor the warlock</a><br>
+        <br> <a href="#">Jean the sorcerer</a><br>
+        <br> <a href="#">Hernesto the cleric</a>
+    </nav>
+</section>
+<section>
+
+    <h2>Contact</h2>
+
+       <form>
+        <fieldset>
+            <legend>
+               Sugestions/Commentary you may like to give. or if you want to share your own stories please fill this form. 
+            </legend>
+            <div>
+                <label>Nombre</label>
+           <input type="text" placeholder="Name" id="Nombre">
+       </div>
+           <div>
+              <label>Favor de declarar tipo</label>
+<select name="Commentary" id="Commentary">
+    <option value="Commentary">Commentary</option>
+    <option value="Sugestion">Sugestion</option>
+</select>
+           </div>
+          <div>
+           <textarea id="Texto"></textarea>
+           <input type="submit" value="Send"> 
+          </div>
+          
+        </fieldset>
+    </form> 
+ </section> 
+    <section>
+        <h2>Credits</h2>
+    </section>
+        <footer>
+            <p>Dnd logo rights reserverd by hasbro and wizards of the cost</p>
+            <p>all right of the stories and usage of the website reserved by Jose Alejandro Gracida</p>
+        </footer>
+</body>
+</html>
